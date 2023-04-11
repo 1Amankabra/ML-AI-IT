9581f3da7fc1ff8a459b14e4eb109c725cd1ef14
amanmahesh <108971073+amanmahesh@users.noreply.github.com>
2023-04-11 14:03:39 +0530

    commit

---
+++ my_file.py
@@ -0,0 +1,14 @@
+c=8
+
+def abc():
+    print("hii")
+
+  
+
+#dunder __start__ 
+
+if __name__ =='__main__':
+    a=int(input('Enter the a'))
+    b=int(input('Enter the b'))
+    print(c*(a+b)) 
+    print('yh main  s call ')
---
+++ newpack/new1.py
@@ -0,0 +1,2 @@
+def xyz():
+    print('xyz')
