f822340f4fc4dee2d54368ddf1b63b68f1cae495
amanmahesh <108971073+amanmahesh@users.noreply.github.com>
2023-05-03 11:58:14 +0530

    commit

--- day_12Pro.py
+++ day_12Pro.py
@@ -4,8 +4,10 @@ from time import sleep
 import numpy as np
 
 fd=cv2.CascadeClassifier(cv2.data.haarcascades+'haarcascade_frontalface_default.xml')
+sd=cv2.CascadeClassifier(cv2.data.haarcascades+'haarcascade_smile.xml')
 vid=cv2.VideoCapture(0)
-while True:
+notCaptured=True
+while notCaptured:
     flag,img=vid.read()
     if flag:
 
@@ -18,16 +20,25 @@ while True:
         #break
 
         np.random.seed(50)
-        colors=[np.random.randint(0,255,(len(faces),3)).tolist() for i in faces]
+        colors=np.random.randint(0,255,(len(faces),3)).tolist() 
         i=0
         for x,y,w,h in faces:
             #x,y,w,h = (250,200,100,100)
             #img_cropped =img[y:y+h,x:x+w,1]
+            face=img_gray[y:y+h,x:x+w].copy()
+            smiles=sd.detectMultiScale(face,scaleFactor=1.1,minNeighbors=5,minSize=(50,50))
             
+            if len(smiles)==1:
+                cv2.imwrite("myselfie.png",img)
+                notCaptured=False
+                break
+
             cv2.rectangle(
                 img,pt1=(x,y),pt2=(x+w,y+h),color=colors[i]
             )
             i+=1
+
+
         cv2.imshow('Preview',img)
         key=cv2.waitKey(1)
         if key==ord('q'):
