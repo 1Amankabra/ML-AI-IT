df5eeea8a0a9b4c1c7ccd728b27bb2697e5027db
amanmahesh <108971073+amanmahesh@users.noreply.github.com>
2023-05-05 10:22:24 +0530

    commit

--- day_12Pro.py
+++ day_12Pro.py
@@ -3,18 +3,23 @@ import matplotlib.pyplot as plt
 from time import sleep
 import numpy as np
 
+#for face recognization
 fd=cv2.CascadeClassifier(cv2.data.haarcascades+'haarcascade_frontalface_default.xml')
+#for smile recognization
 sd=cv2.CascadeClassifier(cv2.data.haarcascades+'haarcascade_smile.xml')
+#video start(camera)
 vid=cv2.VideoCapture(0)
+
 notCaptured=True
 while notCaptured:
-    flag,img=vid.read()
+    flag,img=vid.read()  #read the image
     if flag:
 
         #processing
         img_gray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-
-        faces=fd.detectMultiScale(img_gray,scaleFactor=1.1,minNeighbors=5,minSize=(50,50))
+        
+        #face detect in form
+        faces=fd.detectMultiScale(img_gray,scaleFactor=1.1,minNeighbors=5,minSize=(180,180))
         #th,img_bw=cv2.threshold(img_gray,100,255,cv2.THRESH_BINARY)
        # print(type(img_gray))
         #break
@@ -23,10 +28,11 @@ while notCaptured:
         colors=np.random.randint(0,255,(len(faces),3)).tolist() 
         i=0
         for x,y,w,h in faces:
+            #coordinations 
             #x,y,w,h = (250,200,100,100)
             #img_cropped =img[y:y+h,x:x+w,1]
             face=img_gray[y:y+h,x:x+w].copy()
-            smiles=sd.detectMultiScale(face,scaleFactor=1.1,minNeighbors=5,minSize=(50,50))
+            smiles=sd.detectMultiScale(face,scaleFactor=1.1,minNeighbors=5,minSize=(20,20))
             
             if len(smiles)==1:
                 cv2.imwrite("myselfie.png",img)
