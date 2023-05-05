5fb626cb7f37c3511bd10e18d3e27e21d5f8c91e
amanmahesh <108971073+amanmahesh@users.noreply.github.com>
2023-05-05 10:48:14 +0530

    day13-selfi_machine

--- day_12Pro.py
+++ day_12Pro.py
@@ -11,6 +11,7 @@ sd=cv2.CascadeClassifier(cv2.data.haarcascades+'haarcascade_smile.xml')
 vid=cv2.VideoCapture(0)
 
 notCaptured=True
+seq=0
 while notCaptured:
     flag,img=vid.read()  #read the image
     if flag:
@@ -20,24 +21,24 @@ while notCaptured:
         
         #face detect in form
         faces=fd.detectMultiScale(img_gray,scaleFactor=1.1,minNeighbors=5,minSize=(180,180))
-        #th,img_bw=cv2.threshold(img_gray,100,255,cv2.THRESH_BINARY)
-       # print(type(img_gray))
-        #break
+       
 
         np.random.seed(50)
         colors=np.random.randint(0,255,(len(faces),3)).tolist() 
         i=0
         for x,y,w,h in faces:
             #coordinations 
-            #x,y,w,h = (250,200,100,100)
-            #img_cropped =img[y:y+h,x:x+w,1]
             face=img_gray[y:y+h,x:x+w].copy()
             smiles=sd.detectMultiScale(face,scaleFactor=1.1,minNeighbors=5,minSize=(20,20))
             
             if len(smiles)==1:
-                cv2.imwrite("myselfie.png",img)
-                notCaptured=False
-                break
+                seq+=1
+                if seq==5:
+                    cv2.imwrite("myselfie.png",img)
+                    notCaptured=False
+                    break
+            else:
+                seq=0
 
             cv2.rectangle(
                 img,pt1=(x,y),pt2=(x+w,y+h),color=colors[i]
