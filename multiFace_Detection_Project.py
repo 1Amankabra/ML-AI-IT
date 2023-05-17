a5a5725afefe8eb6aa690b945ad7d134383fa1ef
amanmahesh <108971073+amanmahesh@users.noreply.github.com>
2023-05-17 18:13:47 +0530

    commit

--- multiFace_Detection_Project.py
+++ multiFace_Detection_Project.py
@@ -1,15 +1,15 @@
+#Registered faces compare
 import cv2
 from time import sleep
 import face_recognition as fr
 import pandas as pd
+import numpy as np
+import webbrowser as wb
 
 fd=cv2.CascadeClassifier(cv2.data.haarcascades+'haarcascade_frontalface_default.xml')
 vid=cv2.VideoCapture(0)
-name=input('Enter Your name')
-frameLimit=20
-frameCount=0
-names=[]
-enc=[]
+old_data=pd.read_csv('face_data.csv',index_col=0,sep='|')
+
 while True:
     flag,img=vid.read()
     if flag:
@@ -24,11 +24,15 @@ while True:
             img_face=cv2.resize(img_face,(400,400),cv2.INTER_CUBIC)
             face_encoding=fr.face_encodings(img_face)
             if len(face_encoding)==1:
-                enc.append(face_encoding[0])
-                names.append(name)
-                frameCount +=1
-                if frameCount == frameLimit:
-                    break
+                for ind,entries in old_data.iterrows():
+                    matched=fr.compare_faces(face_encoding,np.array(eval(entries['encoding'])))
+                    confidence=1-fr.face_distance(face_encoding,np.array(eval(entries['encoding'])))
+                    if matched and confidence>0:
+                        print(entries['names'])
+                        cv2.putText(img,entries['names'],(30,30),cv2.FONT_HERSHEY_COMPLEX,1.5,(0,0,255),5)
+                        if entries['names']=='aman':
+                            wb.open_new("https://www.youtube.com/")
+                        break
            # print(face_encoding)
 
         for x,y,w,h in faces:
@@ -42,8 +46,7 @@ while True:
         print('No frames')
         break
     #sleep(0.1)
-data={'names':names,'encoding':enc}
-pd.DataFrame(data).to_csv('face_data.csv')
+
 cv2.destroyAllWindows()
 cv2.waitKey(1)
 vid.release()
